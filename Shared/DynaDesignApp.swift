ee3d3999988ac54184b9c14cafbcdf90ef2fce07
Oblivious <me262thanos@gmail.com>
2021-12-10 05:20:07 +0200

    Initial Commit

---
+++ Shared/DynaDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynaDesignApp.swift
+//  Shared
+//
+//  Created by Thanasis Papapostolou on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DynaDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
