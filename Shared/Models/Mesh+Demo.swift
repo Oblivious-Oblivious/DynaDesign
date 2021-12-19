bd975a11a372a63ae2c4434c3191f74daba43a31
Oblivious <thanos.papap42@gmail.com>
2021-12-19 20:16:52 +0200

    Simplified mesh creation.

--- Shared/Models/Mesh+Demo.swift
+++ Shared/Models/Mesh+Demo.swift
@@ -5,30 +5,16 @@
 //  Created by Thanasis Papapostolou on 10/12/21.
 //
 
-import Foundation
-import CoreGraphics
+import SwiftUI
 
 extension Mesh {
-    /* Angle in radians */
-    private func point_with_center(center: CGPoint, radius: CGFloat, angle: CGFloat) -> CGPoint {
-        let deltax = radius * cos(angle);
-        let deltay = radius * sin(angle);
-        
-        return CGPoint(x: center.x + deltax, y: center.y + deltay);
-    }
-
     static func sample_mesh() -> Mesh {
         let mesh = Mesh();
 
-        [150, 90, 30].forEach { angle in
-            let point = mesh.point_with_center(
-                center: .zero,
-                radius: 250,
-                angle: angle.radians
-            );
-            mesh.add_node(at: point);
+        [50, 100, 150].forEach { angle in
+            mesh.add_node(at: CGPoint(x: angle, y: angle));
         }
-        
+
         return mesh;
     }
 }
