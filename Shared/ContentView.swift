c23a47cdd84a9c54a224eebd06ce86f69594a8eb
Oblivious <thanos.papap42@gmail.com>
2021-12-21 07:47:42 +0200

    Used ContentView.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,19 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        let mesh = MeshController.sample_mesh();
-        let selection = SelectionController();
-        
-        PaneView(
-            mesh: mesh,
-            selection: selection,
-            pane_color: Color("PaneColor")
-        )
-        .onAppear() {
-            /* Remove all scroll indicators */
-            UITableView.appearance().showsVerticalScrollIndicator = false;
-            UITableView.appearance().showsHorizontalScrollIndicator = false;
-        }
+        GridView(
+            projects_list: ProjectModel.sample_list(),
+            options: GridOptions()
+        );
     }
 }
 
