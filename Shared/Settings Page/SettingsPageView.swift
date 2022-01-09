0f2b81fb9ff1bdfe3a9ce3272f898bbf3451b6f9
Oblivious <thanos.papap42@gmail.com>
2022-01-09 10:58:39 +0200

    Fixed colors on settings page.
    TODO -> Need work on more customization options.

--- Shared/Settings Page/SettingsPageView.swift
+++ Shared/Settings Page/SettingsPageView.swift
@@ -9,20 +9,31 @@ import SwiftUI
 
 struct SettingsPageView: View {
     @EnvironmentObject var model_data: ModelData;
+    @Environment(\.presentationMode) var presentation_mode;
 
     var body: some View {
         List {
             HStack {
                 Text("Username")
+                    .foregroundColor(Color("TextColor"))
                     .bold();
                 Divider();
-                TextField("Username", text: $model_data.user_options.username);
+                TextField("Username", text: $model_data.user_options.username)
+                    .foregroundColor(Color("TextColor"))
             }
             
             Toggle(isOn: $model_data.user_options.is_left_handed) {
                 Text("Left Handed")
+                    .foregroundColor(Color("TextColor"))
                     .bold();
             }
+            
+            Spacer();
+            
+            Button("Back") {
+                presentation_mode.wrappedValue.dismiss();
+            }
+            .foregroundColor(.accentColor)
         }
     }
 }
