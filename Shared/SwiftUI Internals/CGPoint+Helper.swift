dee8fed1ded971c91a5f834c4f1a5f9cf1ced355
Oblivious <thanos.papap42@gmail.com>
2022-01-21 14:04:52 +0200

    Removed the need for center alignment.

--- Shared/SwiftUI Internals/CGPoint+Helper.swift
+++ Shared/SwiftUI Internals/CGPoint+Helper.swift
@@ -8,10 +8,6 @@
 import CoreGraphics
 
 extension CGPoint {
-    func center_align(_ parent: CGSize) -> CGPoint {
-        return CGPoint(x: parent.width / 2 + self.x, y: parent.height / 2 + self.y);
-    }
-    
     func translated_by(x: CGFloat, y: CGFloat) -> CGPoint {
         return CGPoint(x: self.x + x, y: self.y + y);
     }
